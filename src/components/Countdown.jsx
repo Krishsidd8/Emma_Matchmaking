7e308edd273c90d3b678b53d9d8822061245b383
Krishsidd8 <krishsidd8@gmail.com>
2025-11-02 01:08:45 -0700

    hi

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,7 +3,7 @@ import "../styles/Countdown.css";
 
 const Countdown = ({ onFinish }) => {
   // Fixed target date
-  const targetDate = new Date('2025-11-02T01:00:00-07:00').getTime();
+  const targetDate = new Date('2025-11-02T01:15:00-07:00').getTime();
   const calculateTimeRemaining = () => {
     const now = new Date().getTime();
     return Math.max(targetDate - now, 0);
