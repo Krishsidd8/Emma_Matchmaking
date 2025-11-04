f7ade1e1d5da8f7d8b6c1c208e282a61d5249bf2
Krishsidd8 <krishsidd8@gmail.com>
2025-11-04 15:09:51 -0800

    fix

--- src/pages/Matchmaking.js
+++ src/pages/Matchmaking.js
@@ -226,7 +226,7 @@ function Matchmaking() {
     <div className="content-card">
       <h2>Waiting for matchmaking...</h2>
       <Countdown
-        targetDate = "2025-11-04T03:15:00-08:00"
+        targetDate = "2025-11-04T03:20:00-08:00"
         onFinish={runMatchmaking}
       />
     </div>
